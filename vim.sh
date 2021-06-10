ec003ea0a3ca38c956bc733de93310d1832b46db
juansegaes <juanse.gaes@gmail.com>
2021-06-10 11:42:19 -0500

    changed mv to cp

--- vim.sh
+++ vim.sh
@@ -1,11 +1,11 @@
 sudo pacman -S vim cmake gcc python3 mono go nodejs jdk-openjdk npm zathura zathura-pdf-mupdf
-mv .vimrc ~/
+cp .vimrc ~/
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +'PlugInstall --sync' +qa
 python3 ~/.vim/plugged/YouCompleteMe/install.py --all
 mkdir ~/.vim/ultisnips
 mkdir ~/.config/zathura
-mv zathurarc ~/.config/zathura
+cp zathurarc ~/.config/zathura
 cp tex.snippets ~/.vim/plugged
 
