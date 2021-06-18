2d41d7fa0297d99acf5e092cc1b4753dcedd5bbe
juansegaes <juanse.gaes@gmail.com>
2021-06-18 10:34:46 -0500

    added init.vim

--- vim.sh
+++ vim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo pacman -S vim cmake gcc python3 mono go nodejs jdk-openjdk npm zathura zathura-pdf-mupdf
+sudo pacman -S vim cmake gcc python3 mono go nodejs jdk-openjdk npm zathura zathura-pdf-mupdf python-pip nvim
 cp .vimrc ~/
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -7,6 +7,8 @@ vim +'PlugInstall --sync' +qa
 python3 ~/.vim/plugged/YouCompleteMe/install.py --all
 mkdir ~/.vim/ultisnips
 mkdir ~/.config/zathura
+mkdir ~/.config/nvim
 cp zathurarc ~/.config/zathura
 cp tex.snippets ~/.vim/plugged
+cp init.vim ~/.config/nvim
 
